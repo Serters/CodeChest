2ec757078c2483eaa5c8019d0af687f756f4786a
Danilo <123840656+DaniloIvk@users.noreply.github.com>
2023-12-01 23:09:40 +0100

    23:09 - javascript update

--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -12,10 +12,20 @@ function draw_main(where) {
 	const m = document.createElement("main");
 	where.appendChild(m);
 
+	// - - - - - - - - - - - - LEFT - - - - - - - - - - - -
 	const left = document.createElement("div");
 	left.className = "left";
 	m.appendChild(left);
 
+	const createSnippet = document.createElement("div");
+	createSnippet.className = "create-snippet";
+	left.appendChild(createSnippet);
+
+	const createSnippetText = document.createElement("h3");
+	createSnippetText.innerText = "Create new snippet";
+	createSnippet.appendChild(createSnippetText);
+
+	// - - - - - - - - - - - - RIGHT - - - - - - - - - - - -
 	const right = document.createElement("div");
 	right.className = "right";
 	m.appendChild(right);
