dac0f2407743c2aae0268c4a3f4478fb43fbf083
Strahinja <strahinja.antic002@gmail.com>
2023-12-19 22:58:15 +0100

    classes

---
+++ front-end/classes/snippet.js
@@ -0,0 +1,37 @@
+class MyClass {
+    constructor(snippet_id, name, code, short_desc, full_desc, favourite, snippet_list_id) {
+      this.snippet_id = snippet_id;
+      this.name = name;
+      this.code = code;
+      this.short_desc = short_desc;
+      this.full_desc = full_desc;
+      this.favourite = favourite;
+      this.snippet_list_id = snippet_list_id;
+    }
+  
+    display(targetDivId) {
+      const outputDiv = document.getElementById(targetDivId);
+      if (outputDiv) {
+        outputDiv.innerHTML = `
+          <p>Snippet Information:</p>
+          <ul>
+            <li>Snippet ID: ${this.snippet_id}</li>
+            <li>Name: ${this.name}</li>
+            <li>Code: ${this.code}</li>
+            <li>Short Description: ${this.short_desc}</li>
+            <li>Full Description: ${this.full_desc}</li>
+            <li>Favourite: ${this.favourite ? 'Yes' : 'No'}</li>
+            <li>Snippet List ID: ${this.snippet_list_id}</li>
+          </ul>
+        `;
+      } else {
+        console.error(`Target div with id "${targetDivId}" not found.`);
+      }
+    }
+  }
+  
+  // Export the MyClass class for use in other files (optional)
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = MyClass;
+  }
+  
---
+++ front-end/classes/snippet_list.js
@@ -0,0 +1,36 @@
+class snippet_list {
+    constructor(snippet_list_id, max_storage, user_id, snippet_count, snippets) {
+      this.snippet_list_id = snippet_list_id;
+      this.max_storage = max_storage;
+      this.user_id = user_id;
+      this.snippet_count = snippet_count;
+      this.snippets = snippets || [];
+    }
+  
+    display(targetDivId) {
+      const outputDiv = document.getElementById(targetDivId);
+      if (outputDiv) {
+        outputDiv.innerHTML = `
+          <p>Snippet List Information:</p>
+          <ul>
+            <li>Snippet List ID: ${this.snippet_list_id}</li>
+            <li>Max Storage: ${this.max_storage}</li>
+            <li>User ID: ${this.user_id}</li>
+            <li>Snippet Count: ${this.snippet_count}</li>
+            <li>Snippets:</li>
+            <ul>
+              ${this.snippets.map(snippet => `<li>${snippet.name}</li>`).join('')}
+            </ul>
+          </ul>
+        `;
+      } else {
+        console.error(`Target div with id "${targetDivId}" not found.`);
+      }
+    }
+  }
+  
+  // Export the SnippetList class for use in other files (optional)
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = snippet_list;
+  }
+  
