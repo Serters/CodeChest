9e4b260c36e9dc1f73bb56406ac09b88da1506dd
Danilo <123840656+DaniloIvk@users.noreply.github.com>
2023-12-21 18:13:21 +0100

    e

--- testing/main_test.js
+++ testing/main_test.js
@@ -4,34 +4,50 @@ import { user } from "../front-end/classes/user.js";
 
 const snippet_list_default = new snippet_list(1, 100, 1, []);
 
-function fetchAndProcessSnippets() {
-	// Make a GET request to the Flask serve
+async function fetchAndProcessSnippets() {
+    let sl = [];
 
-	fetch("http://127.0.0.1:5000/snippets")
-		.then((response) => response.json())
-		.then((data) => {
-			// Process the data and create snippet objects
-			const snippetsData = data.snippet;
+    try {
+        const response = await fetch("http://127.0.0.1:5000/snippets");
+        const data = await response.json();
 
-			// Assuming you have a function to create a snippet object
-			snippetsData.forEach((snippetData) => {
-				const snippetObject = createSnippetObjectFromData(snippetData);
-				snippet_list_default.append_snippet(snippetObject);
-			});
-			snippet_list_default.display(document.body);
-		})
-		.catch((error) => console.error("Error fetching snippets:", error));
+        const snippetsData = data.snippet;
+
+        snippetsData.forEach((snippetData) => {
+            const snippet = createSnippetObjectFromData(snippetData);
+            sl.push(snippet);
+        });
+
+        snippet_list_default.snippets = sl;
+        // snippet_list_default.display(document.body);
+
+        console.log(sl);
+        
+    } catch (error) {
+        console.error("Error fetching snippets:", error);
+    }
+
+    return sl;
 }
+
 function createSnippetObjectFromData(data) {
-	return new snippet(
-		data[0],
-		data[1],
-		data[2],
-		data[3],
-		data[4],
-		data[5],
-		data[6]
-	);
+    return new snippet(
+        data[0],
+        data[1],
+        data[2],
+        data[3],
+        data[4],
+        data[5],
+        data[6]
+    );
 }
 
-fetchAndProcessSnippets();
+// Use an async function to wait for the fetch operation to complete
+async function initializeSnippetList() {
+    const snippetListData = await fetchAndProcessSnippets();
+    const snippet_list_new = new snippet_list(1, 100, 1, snippetListData);
+    snippet_list_new.display(document.body);
+}
+
+// Call the async function to initialize and display the snippet list
+initializeSnippetList();
